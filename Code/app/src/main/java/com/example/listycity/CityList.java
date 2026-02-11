82377af77b094fc1739a3f26fe6c0e33212c413e
Tina Lin <tinalin@Tinas-MacBook-Air-2.local>
2026-02-10 17:25:28 -0700

    initial creation

---
+++ Code/app/src/main/java/com/example/listycity/CityList.java
@@ -0,0 +1,11 @@
+package com.example.listycity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is a class that keeps a list of city objects
+*/
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+}
